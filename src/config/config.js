5d725a6a43c14f2fb8b8b5970714fa57b9860fea
rigolinux <rigoberto7567@gmail.com>
2023-08-31 18:03:40 -0600

    wompi auth

--- src/config/config.js
+++ src/config/config.js
@@ -6,4 +6,6 @@ dotenv.config();
 export const EnvConfig = {
     port: process.env.PORT || 3000,
     SecretKey: process.env.TOKEN_SUPABESE_SECRET,
+    Wompi_KEY: process.env.WOMPI_API_BASE,
+    Wompi_SECRET: process.env.WOMPI_API_KEY,
 }
--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,13 +1,47 @@
-
+import axios from "axios";
+import { EnvConfig} from "../config/config.js";
 
 
 
 export const createCheckoutSession = async (req, res) => {
+    try {
+        const requestBody = new URLSearchParams();
+        requestBody.append('grant_type', 'client_credentials');
+        requestBody.append('audience', 'wompi_api');
+        requestBody.append('client_id', EnvConfig.Wompi_KEY);
+        requestBody.append('client_secret', EnvConfig.Wompi_SECRET);
 
-    res.json({ message: 'Hello World' });
+        const config = {
+            method: 'post',
+            url: 'https://id.wompi.sv/connect/token',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            data: requestBody.toString(), 
+        };
+
+        const response = await axios(config);
+        console.log(response.data);
+        res.json({ message: 'Hello World' });
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({ message: err.message });
+    }
+
+    
 };
 
 export const successPayment = async (req, res) => {
+    try{
+        //req.body.products has the products
+        
+
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({ message: err.message });
+    }
     
         res.json({ message: 'Hello World' });
 };
