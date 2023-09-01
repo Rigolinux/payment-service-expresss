4f99302c830c039aba76cd887849eca42bf1d7d5
rigolinux <rigoberto7567@gmail.com>
2023-09-01 13:56:27 -0600

    setup paypal enviroment variables

--- src/config/config.js
+++ src/config/config.js
@@ -8,4 +8,8 @@ export const EnvConfig = {
     SecretKey: process.env.TOKEN_SUPABESE_SECRET,
     Wompi_KEY: process.env.WOMPI_API_BASE,
     Wompi_SECRET: process.env.WOMPI_API_KEY,
+    HostPort : process.env.HOST_PORT,
+    Paypal_url: process.env.PAYPAL_URL,
+    Paypal_client_id: process.env.PAYPAL_CLIENT_KEY,
+    Paypal_client_secret: process.env.PAYPAL_SECRET_KEY,
 }
