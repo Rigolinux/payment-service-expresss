0e6d99d1d2c2c14923774144beef4671a3543ab2
rigolinux <rigoberto7567@gmail.com>
2023-10-17 18:09:40 -0600

    enable cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import  express from 'express';
 import paymentroute from './routes/payment.routes.js';
-
+import cors from 'cors';
 
 import { EnvConfig } from './config/config.js';
 
 const app = express();
 
 app.use(express.json());
+
+//enable cors
+app.use(cors());
 
 app.use('/api', paymentroute);
 
